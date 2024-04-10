d5178296f7f3717b51a4253beed4f2336ec4783e
AISTRAGEN <orryn391@gmail.com>
2024-04-11 00:32:33 +0200

    miniUp

--- C++/Labs/lab7/task 1.3/header.h
+++ C++/Labs/lab7/task 1.3/header.h
@@ -4,15 +4,13 @@
 #include <iostream>
 #include <cmath>
 
-
-
 namespace mySpace
 {
-    typedef double (*functionPtr)(double&, double&);
-      void print(double a, double b, functionPtr function)
-      {
+    typedef double (*functionPtr)(double, double);
+    void print(double a, double b, functionPtr function)
+    {
         std::cout << "Arguments: a = " << a << ", b = " << b << std::endl;
         std::cout << "Function result: " << function(a, b) << std::endl;
-      }
+    }
 }
 #endif
--- C++/Labs/lab7/task 1.3/main.cpp
+++ C++/Labs/lab7/task 1.3/main.cpp
@@ -8,16 +8,14 @@ void inputData(double &a, double &b)
    std::cin >> a;
    std::cout << "Введіть значення b" << std::endl;
    std::cin >> b;
-
 }
 
 void outputData(double &a, double &b,  functionPtr function)
 {
     std::cout << "Сума а i b = " << function(a,b) << std::endl;
-   
 }
 
-double funcion(double &a, double &b)
+double funcion(double a, double b)
 {
   double result = pow(a, 2) + pow (b, 2);
   return result;
@@ -30,12 +28,11 @@ int main()
     outputData(a, b, funcion);
     functionPtr functions[] = {&pow, &hypot, &fmax, &fmin, &funcion};
 
-    for (int i = 0; i < sizeof(functions) / sizeof(functions[0]); ++i) 
+   for (int i = 0; i < static_cast<int>(sizeof(functions) / sizeof(functions[0])); ++i)
     {
         print(a, b, functions[i]);
     }
 
     return 0;
 }
-
 
